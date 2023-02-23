44c6ebad6416b89b25da7a7f7153a0274f44475d
inespr <iperrom@g.educaand.es>
2023-02-23 12:15:53 +0100

    Change icon PWA

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      includeAssets: ["MicrosoftTeams-image.png"],
+      includeAssets: ["dictionary.png"],
       manifest: {
         name: "Dictionary_web_api",
         short_name: "Dictionary",
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "MicrosoftTeams-image.png",
+            src: "dictionary.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "MicrosoftTeams-image.png",
+            src: "dictionary.png",
             sizes: "512x512",
             type: "image/png",
           },
